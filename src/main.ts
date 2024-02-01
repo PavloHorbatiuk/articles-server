1b5d989efc5b2427ab291782304c2cf5cfb35d58
Pavlo Horbatiuk <pavlohorbatiuk@gmail.com>
2024-02-02 00:27:41 +0200

    with arq

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,13 @@ async function bootstrap() {
 
 	app.enableCors();
 
+	app.use((req, res, next) => {
+		res.header('Access-Control-Allow-Origin', '*');
+		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+		res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+		next();
+	  })
+
 	const PORT = process.env.PORT || 5000;
 	app.useGlobalPipes(new ValidationPipe())
 	await app.listen(PORT);
